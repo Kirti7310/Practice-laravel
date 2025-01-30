b288ee3a210b95d3348b88bd7e4b229862d9cc51
kirti.karapurkar@anant.co.in <kirti.karapurkar@anant.co.in>
2025-01-30 12:15:25 +0530

    Inital commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,28 @@
+<div>
+
+<x-message-banner msg="User login successfully!"  class="success"/>
+<x-message-banner msg="Password Isnt 8 Characters!" class="error-war"/>
+
+
+</div>
+
+<style>
+
+.success
+{
+    background-color: green;
+    border-radius: 12px;
+    padding: 5px;
+    margin-bottom: 20px;
+}
+
+.error-war{
+    background-color: red;
+    border-radius: 12px;
+    padding: 5px;
+
+}
+
+
+
+</style>
---
+++ resources/views/components/message-banner.blade.php
@@ -0,0 +1,5 @@
+
+<div>
+    <span class={{$class}}>{{$msg}}</span>
+    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+</div>
