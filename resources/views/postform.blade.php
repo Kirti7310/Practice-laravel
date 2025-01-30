abf6d154e1cb08f11fc403fa945bff0ae3fe6e0c
kirti.karapurkar@anant.co.in <kirti.karapurkar@anant.co.in>
2025-01-30 12:54:29 +0530

    Changes Form

---
+++ resources/views/postform.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post Form</title>
+</head>
+<body>
+<h1> Register Here </h1>
+<form action="" method="POST">
+
+<div class="input-wrapper">
+    <label for="name">Name:</label>
+    <input type="text" name="name" placeholder="Enter Your Name">
+</div>
+
+<div class="input-wrapper">
+    <label for="email">Email:</label>
+    <input type="email" name="email" placeholder="Enter Your Email">
+</div>
+
+<div class="input-wrapper">
+    <label for="Password">Password:</label>
+    <input type="text" name="password" placeholder="Enter Your Password">
+</div>
+
+<div class="input-wrapper">
+    <label for="Confirm-Password">Confirm Password:</label>
+    <input type="text" name="confirmpass" placeholder="Re-Enter Your Password">
+</div>
+
+<div class="input-wrapper">
+
+<input type="checkbox" name="cond" placeholder="" id="confirm">
+    <label for="confirm">I agree to the Terms & Conditions Provided Below:</label>
+</div>
+
+
+
+</form>
+    
+</body>
+</html>
